a4fa27c6b62a05594d3534a2c494d91e6cb2f6f2
smy5152 <shabbiryamani@gmail.com>
2021-04-25 03:09:44 +0200

    v1: Friendships model rubocop errors for ruby v2.6 code syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array + inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
+    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
     friends_array.compact
   end
 
@@ -29,7 +29,7 @@ class User < ApplicationRecord
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find { |friendship| friendship.user == user }
+    friendship = inverse_friendships.find { |my_friendship| my_friendship.user == user }
     friendship.confirmed = true
     friendship.save
   end
